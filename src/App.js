2ee05abc53e082b8c266178186e651c7ba4843ce
Ahmed Waseem <AhmedWaseemR@aucegypt.edu>
2023-12-30 21:40:10 +0200

    3d model

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Suspense } from 'react';
+import { Canvas, useFrame, useThree, useLoader} from 'react-three-fiber';
+import { MeshStandardMaterial } from 'three';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { OrbitControls} from '@react-three/drei';
+import model from './assets/vulgato.obj';
+
+
+function Model({ url }) {
+  const obj = useLoader(OBJLoader, url);
+  const { camera } = useThree();
+
+  useFrame(() => {
+    camera.lookAt(obj.position);
+  });
+
+  // Create a material and set its color.
+  const material = new MeshStandardMaterial({ color: 0x60e1fa }); // Red color.
+
+  // Apply the material to all children of the model.
+  obj.traverse((child) => {
+    if (child.isMesh) {
+      child.material = material;
+    }
+  });
+
+  return <primitive object={obj} />;
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Canvas camera={{ position: [0, 0, 10] }}>
+          <ambientLight />
+          <Suspense fallback={null}>
+            <Model url={model} />
+          </Suspense>
+          <OrbitControls />
+        </Canvas>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
       </header>
     </div>
   );
